a2e13ab846ea536dda46cf3f81e18ffea1d4a560
Freddie <f.varnals@gmail.com>
2019-09-17 17:48:38 +0100

    Error raised when any item in soundwave array is not an integer

--- lib/band_pass_filter.rb
+++ lib/band_pass_filter.rb
@@ -1,6 +1,11 @@
 class BandPassFilter
 
-  def check_for_errors
-    raise "Error in soundwave"
+  def check_for_errors(array)
+    array.each do
+      |frequency|
+      if !frequency.is_a? Integer
+        raise "Error in soundwave"
+      end
+    end
   end
 end
--- spec/band_pass_filter_spec.rb
+++ spec/band_pass_filter_spec.rb
@@ -4,7 +4,13 @@ describe BandPassFilter do
   subject(:band_pass_filter) {described_class.new}
   describe '#check_for_errors' do
     it 'outputs "Error in soundwave" when given ["a"] as input array' do
-      expect {band_pass_filter.check_for_errors}.to raise_error("Error in soundwave")
+      expect {band_pass_filter.check_for_errors(["a"])}.to raise_error("Error in soundwave")
+    end
+    it 'does not raise error when given [1,2] as input array' do
+      expect {band_pass_filter.check_for_errors([1,2])}.not_to raise_error("Error in soundwave")
+    end
+    it 'outputs "Error in soundwave" when given [33,44,23,"a",5] as input array' do
+      expect {band_pass_filter.check_for_errors([33,44,23,"a",5])}.to raise_error("Error in soundwave")
     end
   end
 end
